47ba8e7d6560bed90e341191fd81734f70c600bc
Tolan Beniamin <39988307+xTbeniaminTx@users.noreply.github.com>
2021-12-18 13:57:46 +0100

    signup page and controler

--- src/main/java/com/front/bank/userfrontbank/controller/HomeController.java
+++ src/main/java/com/front/bank/userfrontbank/controller/HomeController.java
@@ -1,7 +1,12 @@
 package com.front.bank.userfrontbank.controller;
 
+import com.front.bank.userfrontbank.domain.User;
+import java.util.HashSet;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
@@ -15,5 +20,39 @@ public class HomeController {
   public String index() {
     return "index";
   }
+
+  @RequestMapping(value = "/signup", method = RequestMethod.GET)
+  public String signup(Model model) {
+    User user = new User();
+
+    model.addAttribute("user", user);
+
+    return "signup";
+  }
+
+  @RequestMapping(value = "/signup", method = RequestMethod.POST)
+  public void signupPost(@ModelAttribute("user") User user,  Model model) {
+
+//    if(userService.checkUserExists(user.getUsername(), user.getEmail()))  {
+//
+//      if (userService.checkEmailExists(user.getEmail())) {
+//        model.addAttribute("emailExists", true);
+//      }
+//
+//      if (userService.checkUsernameExists(user.getUsername())) {
+//        model.addAttribute("usernameExists", true);
+//      }
+//
+//      return "signup";
+//    } else {
+//      Set<UserRole> userRoles = new HashSet<>();
+//      userRoles.add(new UserRole(user, roleDao.findByName("ROLE_USER")));
+//
+//      userService.createUser(user, userRoles);
+//
+//      return "redirect:/";
+//    }
+  }
+
 
 }
